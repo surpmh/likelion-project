72b82a71674c961856d55313847c860348c8092a
surpmh <cdd8816@gmail.com>
2022-12-20 13:18:26 +0900

    refactor: ResponseEntity로 리팩토링

--- src/main/java/com/likelion/likelionproject/controller/HelloController.java
+++ src/main/java/com/likelion/likelionproject/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.likelion.likelionproject.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
     }
 }
