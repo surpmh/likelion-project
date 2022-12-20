1e2b1d14c9e109577fa919de458ea5c1ce26cb37
surpmh <cdd8816@gmail.com>
2022-12-20 11:07:56 +0900

    feat: Swagger 적용

---
+++ src/main/java/com/likelion/likelionproject/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.likelion.likelionproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
