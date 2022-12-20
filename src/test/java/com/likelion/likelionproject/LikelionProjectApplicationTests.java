8446e4cdcd9c3c184784eef64c64e295a435e2b2
surpmh <cdd8816@gmail.com>
2022-12-20 11:28:40 +0900

    test: contextLoads 제거

--- src/test/java/com/likelion/likelionproject/LikelionProjectApplicationTests.java
+++ src/test/java/com/likelion/likelionproject/LikelionProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LikelionProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
