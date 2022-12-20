9312d88b617e8930a5425535e5eaad1cd08f54ce
surpmh <cdd8816@gmail.com>
2022-12-20 10:46:43 +0900

    새 프로젝트 생성

---
+++ src/main/java/com/likelion/likelionproject/LikelionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likelionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/likelionproject/LikelionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likelionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
