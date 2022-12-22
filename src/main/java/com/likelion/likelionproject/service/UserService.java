da2562ebf925f199536ece7f60d849fc88aac4f2
surpmh <cdd8816@gmail.com>
2022-12-22 11:49:49 +0900

    feat: 비밀번호를 암호화하여 저장

--- src/main/java/com/likelion/likelionproject/service/UserService.java
+++ src/main/java/com/likelion/likelionproject/service/UserService.java
@@ -8,21 +8,29 @@ import com.likelion.likelionproject.enums.ErrorCode;
 import com.likelion.likelionproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
     public UserDto join(UserJoinRequest request) {
         userRepository.findByUserName(request.getUserName())
                 .ifPresent(user -> {
                     throw new AppException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated.", request.getUserName()));
                 });
 
-        User savedUser = userRepository.save(request.toEntity());
+        User user = User.builder()
+                .userName(request.getUserName())
+                .password(encoder.encode(request.getPassword()))
+                .build();
+
+        User savedUser = userRepository.save(user);
 
         return UserDto.builder()
                 .id(savedUser.getId())
