add778713e96abbd53586f179e5a03a560b31409
surpmh <cdd8816@gmail.com>
2022-12-23 09:48:18 +0900

    refactor: 로그인 Response 리팩토링

--- src/main/java/com/likelion/likelionproject/controller/UserController.java
+++ src/main/java/com/likelion/likelionproject/controller/UserController.java
@@ -4,7 +4,6 @@ import com.likelion.likelionproject.dto.*;
 import com.likelion.likelionproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
