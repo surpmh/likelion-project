a43749ed36128590263ab5f91a5641377e8855cf
surpmh <cdd8816@gmail.com>
2022-12-23 11:06:43 +0900

    feat: ErrorCode 추가

--- src/main/java/com/likelion/likelionproject/enums/ErrorCode.java
+++ src/main/java/com/likelion/likelionproject/enums/ErrorCode.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Duplicate UserName."),
-    NOT_FOUND_USER_NAME(HttpStatus.NOT_FOUND, "Not Found UserName"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid Password");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"Not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러");
 
     private final HttpStatus httpStatus;
     private final String message;
