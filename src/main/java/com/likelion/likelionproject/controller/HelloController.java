2c0e76b4a087f7a976dc87ccd9e01f035a37501c
surpmh <cdd8816@gmail.com>
2022-12-26 09:03:45 +0900

    test: 오늘의 단어 변경

--- src/main/java/com/likelion/likelionproject/controller/HelloController.java
+++ src/main/java/com/likelion/likelionproject/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("popin");
+        return ResponseEntity.ok().body("happy_new_year");
     }
 }
