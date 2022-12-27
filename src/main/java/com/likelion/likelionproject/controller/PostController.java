06dfed332fc2a58ae01497337dd19235268e1a7f
surpmh <cdd8816@gmail.com>
2022-12-27 09:24:35 +0900

    feat: 포스트 상세 조회 기능 추가

--- src/main/java/com/likelion/likelionproject/controller/PostController.java
+++ src/main/java/com/likelion/likelionproject/controller/PostController.java
@@ -1,16 +1,14 @@
 package com.likelion.likelionproject.controller;
 
 import com.likelion.likelionproject.dto.PostCreateRequest;
-import com.likelion.likelionproject.dto.PostCreateResponse;
+import com.likelion.likelionproject.dto.PostResponse;
 import com.likelion.likelionproject.dto.PostDto;
 import com.likelion.likelionproject.dto.Response;
+import com.likelion.likelionproject.entity.Post;
 import com.likelion.likelionproject.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -19,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
     private final PostService postService;
 
+    // 포스트 상세
+    @GetMapping("/{id}")
+    public Response<PostReadResponse> detail(@PathVariable Long id) {
+        Post post = postService.detail(id);
+        PostReadResponse response = PostReadResponse.fromEntity(post);
+        return Response.success(response);
+    }
+
     // 포스트 등록
     @PostMapping("/create")
-    public Response<PostCreateResponse> create(@RequestBody PostCreateRequest postCreateRequest) {
+    public Response<PostResponse> create(@RequestBody PostCreateRequest postCreateRequest) {
         PostDto postDto = postService.create(postCreateRequest);
-        return Response.success(new PostCreateResponse("포스트 등록 완료", postDto.getId()));
+        return Response.success(new PostResponse("포스트 등록 완료", postDto.getId()));
     }
 }
--- src/main/java/com/likelion/likelionproject/service/PostService.java
+++ src/main/java/com/likelion/likelionproject/service/PostService.java
@@ -3,6 +3,8 @@ package com.likelion.likelionproject.service;
 import com.likelion.likelionproject.dto.PostCreateRequest;
 import com.likelion.likelionproject.dto.PostDto;
 import com.likelion.likelionproject.entity.Post;
+import com.likelion.likelionproject.enums.ErrorCode;
+import com.likelion.likelionproject.exception.AppException;
 import com.likelion.likelionproject.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+
+    public Post detail(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, "Post not founded."));
+    }
 
     public PostDto create(PostCreateRequest request) {
         Post savedPost = postRepository.save(request.toEntity());
