b26f4a51670bbb21692f465147b48b7cc4523d40
surpmh <cdd8816@gmail.com>
2022-12-27 09:33:15 +0900

    feat: 포스트 상세 조회 기능 구현

---
+++ src/main/java/com/likelion/likelionproject/dto/PostReadResponse.java
@@ -0,0 +1,31 @@
+package com.likelion.likelionproject.dto;
+
+import com.likelion.likelionproject.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostReadResponse {
+    private Long id;
+    private String title;
+    private String body;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedAt;
+
+    public static PostReadResponse fromEntity(Post post) {
+        return PostReadResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .body(post.getBody())
+                .createdAt(post.getCreatedAt())
+                .lastModifiedAt(post.getLastModifiedAt())
+                .build();
+    }
+}
