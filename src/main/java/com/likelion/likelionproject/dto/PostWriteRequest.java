21185d7eaf831ab47540d66e7f6dd183b527570e
surpmh <cdd8816@gmail.com>
2022-12-27 09:26:30 +0900

    refactor: 이름 변경

--- src/main/java/com/likelion/likelionproject/dto/PostWriteRequest.java
+++ src/main/java/com/likelion/likelionproject/dto/PostWriteRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostCreateRequest {
+public class PostWriteRequest {
     private String title;
     private String body;
 
