6e0d102120dd8d855a3857a8d02e9bc66a0fe799
surpmh <cdd8816@gmail.com>
2022-12-27 09:43:03 +0900

    feat: 빌더 패턴 추가

--- src/main/java/com/likelion/likelionproject/dto/UserLoginRequest.java
+++ src/main/java/com/likelion/likelionproject/dto/UserLoginRequest.java
@@ -1,9 +1,13 @@
 package com.likelion.likelionproject.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequest {
     private String userName;
