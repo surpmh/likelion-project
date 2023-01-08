edf427129ead6fc4faadca440cedb3588907140d
surpmh <cdd8816@gmail.com>
2023-01-08 11:59:48 +0900

    feat: SwaggerInfo 추가

--- src/main/java/com/likelion/likelionproject/configuration/SwaggerConfig.java
+++ src/main/java/com/likelion/likelionproject/configuration/SwaggerConfig.java
@@ -2,11 +2,10 @@ package com.likelion.likelionproject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -19,6 +18,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
@@ -26,6 +27,14 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("MutSa SNS API 명세서")
+                .version("0.1")
+                .build();
+    }
+
 
     private SecurityContext securityContext() {
         return SecurityContext.builder()
